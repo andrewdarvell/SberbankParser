753a59b5feee8f1bee3d42b5349961e72d4a3fd9
andrewdarvell <andrew@darvell.ru>
2014-07-28 17:43:42 +0400

    first commit

---
+++ src/ru/darvell/sberparser/main/SberbankParser.java
@@ -0,0 +1,15 @@
+package ru.darvell.sberparser.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SberbankParser extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
